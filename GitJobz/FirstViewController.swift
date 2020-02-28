5b3755915d68b002fdb269b9c066da2388d53d37
Erison Veshi <erisonve@gmail.com>
2020-02-28 14:59:24 +0100

    Initial Commit

---
+++ GitJobz/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GitJobz
+//
+//  Created by er on 2/28/20.
+//  Copyright © 2020 b4sht4. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitJobz/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GitJobz
+//
+//  Created by er on 2/28/20.
+//  Copyright © 2020 b4sht4. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
